59bc60d9bef245fbd2edc9e58d1243d16bdfc726
dechristol <dudu.christo@gmail.com>
2021-03-19 14:35:22 +0100

    Add CORS supports and failure Repository

--- src/main/java/dataenrichment/controllers/FailureController.java
+++ src/main/java/dataenrichment/controllers/FailureController.java
@@ -1,4 +1,23 @@
 package dataenrichment.controllers;
 
+import dataenrichment.entities.Failure;
+import dataenrichment.services.FailureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
 public class FailureController {
+
+    @Autowired
+    FailureService failureService;
+
+    @GetMapping("/failures")
+    public List<Failure> getFailures() {
+        return failureService.getFailures();
+    }
 }
--- src/main/java/dataenrichment/services/FailureService.java
+++ src/main/java/dataenrichment/services/FailureService.java
@@ -1,4 +1,18 @@
 package dataenrichment.services;
 
+import dataenrichment.entities.Failure;
+import dataenrichment.repository.FailureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FailureService {
+
+    @Autowired
+    private FailureRepository failureRepository;
+    public List<Failure> getFailures() {
+        return failureRepository.findAll();
+    }
 }
